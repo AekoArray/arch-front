b078ba7d88667e36d9bd7b8a89b0dce26ddda79b
ShGulnaz <shakirova139@gmail.com>
2021-04-09 23:57:13 +0300

    fix card and add basket in constructor

--- patterns/card.js
+++ patterns/card.js
@@ -1,17 +1,18 @@
 //Strategy
 //class for defining strategies for different cards
 class CardStrategy {
-    constructor(card) {
+    constructor(card, basket) {
         this.card = card
+        this.basket = basket
     }
 
     //method for determining the strategy, depending on the chosen card
     chooseStrategy(){
         switch (this.card) {
             case "SilverCardStrategy":
-                return new SilverCardStrategy()
+                return new SilverCardStrategy(this.basket)
             case "GoldCardStrategy":
-                return new GoldCardStrategy()
+                return new GoldCardStrategy(this.basket)
             default:
                 return new NoCardStrategy()
         }
@@ -19,22 +20,26 @@ class CardStrategy {
 }
 //strategy for the silver card
 class SilverCardStrategy{
+    constructor(basket) {
+        this.basket = basket;
+    }
 
     //method for getting 10% discount
     makeDiscount() {
-        myBasket.setTotalPrice(myBasket.getTotalPrice() * 0.9)
+        this.basket.setTotalPrice(this.basket.getTotalPrice() * 0.9)
     }
 }
 
 //strategy for the gold card
 class GoldCardStrategy{
-    constructor() {
+    constructor(basket) {
         this.points = 0;
+        this.basket = basket;
     }
 
     //method for getting 20% discount
     makeDiscount() {
-        myBasket.setTotalPrice(myBasket.getTotalPrice() * 0.8)
+        this.basket.setTotalPrice(this.basket.getTotalPrice() * 0.8)
         this.getPoints();
     }
 
@@ -46,7 +51,7 @@ class GoldCardStrategy{
 
     //method for using points on a card
     usePoints() {
-        myBasket.setTotalPrice(myBasket.getTotalPrice() - this.points)
+        this.basket.setTotalPrice(this.basket.getTotalPrice() - this.points)
         this.points = 0
     }
 }
