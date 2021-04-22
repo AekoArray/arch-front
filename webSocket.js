de834d863ff9685adfbafe23672c28bcc7f2e343
DinoBambino69 <deltaxxl69@mail.ru>
2021-04-22 21:37:23 +0300

    add comment

--- webSocket.js
+++ webSocket.js
@@ -1,11 +1,9 @@
 async function webSocketStart() {
 
+    //Opening a WebSocket connection
     const socket = new WebSocket("wss://localhost:8080");
 
-    socket.onopen = function (e) {
-        console.log("WebSocket start!");
-    };
-
+    //Getting a message-display data in li#categories
     socket.onmessage = function (event) {
         let answer = event.data;
 
